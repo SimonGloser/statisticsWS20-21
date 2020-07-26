e344fd13331f418c087be886feb2b40717c938bd
SimonGloser <simon.gloser@googlemail.com>
2020-07-26 18:59:25 +0200

    add find samples

--- sheet1/sheet1.R
+++ sheet1/sheet1.R
@@ -10,3 +10,7 @@ log10(200)*sin(pi/4)
 
 # the cumulative sum of the numbers 1,3,18,20,2
 cumsum(c(1,3,18,20,2))
+
+# find 10 numbers between 0 and 20 rounded to the nearest
+sample(x = 0:20, size = 10, replace = FALSE)
+
